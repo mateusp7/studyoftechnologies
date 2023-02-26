080544c33d88db67dc12b00daf4140695e54f20e
Mateus Raizen <mateusdev1337@gmail.com>
2023-02-26 18:45:31 -0300

    First Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client/core"
+import { useQuery } from "@apollo/client/react/hooks"
+import { NewUserForm } from "./components/NewUserForm"
+
+type User = {
+  id: string
+  name: string
+}
+
+export const GET_USER = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`
+
+function App() {
+  const { data, loading } = useQuery<{ users: User[] }>(GET_USER)
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+  return (
+    <div>
+      <ul>
+        {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+      <NewUserForm />
+    </div>
+  )
+}
+
+export default App
